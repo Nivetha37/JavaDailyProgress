c65f342b75770e398d1f34c122408d9429fe807d
Nivetha37 <124027942+Nivetha37@users.noreply.github.com>
2024-02-20 11:43:11 +0530

    Create Fitness.java
    Problem Statement : Chef wants to become fit for which he decided to walk to the office and return home by walking.
    It is known that Chef's office is X km away from his home.
    If his office is open on 5 days in a week, find the number of kilometres Chef travels through office trips in a week.
    
    Hint
    
    Return trips imply that 2*X distance is travelled each day

---
+++ Fitness.java
@@ -0,0 +1,20 @@
+// Update the code below to solve the problem
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+	    Scanner read = new Scanner(System.in);
+		
+		int t = read.nextInt();
+		//Update your code below this line to solve the problem
+		for(int i=0;i<t;i++){
+		    
+		    int x =read.nextInt();
+		    System.out.println(x*2*5);   
+		}
+	}
+}
+//input:2 1 3
+//output: 10 30
