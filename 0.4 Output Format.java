2ae778b648a7d41dc592ecd03005fd428233146b
Nivetha37 <124027942+Nivetha37@users.noreply.github.com>
2024-03-05 11:05:52 +0530

    0.4 Output Format.java

--- 0.4 Output Format.java
+++ 0.4 Output Format.java
@@ -2,7 +2,9 @@
 
 In each line of output there should be two columns:
 The first column contains the String and is left justified using exactly  characters.
-The second column contains the integer, expressed in exactly  digits; if the original input has less than three digits, you must pad your output's leading digits with zeroes.*\
+The second column contains the integer, expressed in exactly  digits; if the original input has less than three digits, you must pad your output's leading digits with zeroes.
+
+*\
 
 \\my code
 import java.util.Scanner;
@@ -43,6 +45,7 @@ public class Solution {
 
     }
 }
+
 
 \**
 
