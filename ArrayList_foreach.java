fa6a6bfb11bae83cc702ee993c84c291fcad351f
Nivetha37 <124027942+Nivetha37@users.noreply.github.com>
2024-03-28 21:28:02 +0530

    Create ArrayList_foreach.java

---
+++ ArrayList_foreach.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+        ArrayList<String> names = new ArrayList<String>();
+        
+        names.add("Alice");
+        names.add("Bob");
+        names.add("Charlie");
+        names.add("Dave");
+        
+        for(String name : names ){
+            System.out.println(name);
+        }
+	}
+}
