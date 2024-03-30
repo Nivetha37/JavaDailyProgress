8d3ea36aa73635a131a94a3ea2156ba474d0ee5d
Nivetha37 <124027942+Nivetha37@users.noreply.github.com>
2024-03-30 16:35:15 +0530

    Create 42 Apples_&_Oranges.java

---
+++ 42 Apples_&_Oranges.java
@@ -0,0 +1,112 @@
+/*
+Complete the countApplesAndOranges function in the editor below. It should print the number of apples and oranges that land on Sam's house, each on a separate line.
+
+countApplesAndOranges has the following parameter(s):
+
+s: integer, starting point of Sam's house location.
+t: integer, ending location of Sam's house location.
+a: integer, location of the Apple tree.
+b: integer, location of the Orange tree.
+apples[]: integer array, distances at which each apple falls from the tree.
+oranges[]: integer array, distances at which each orange falls from the tree.
+
+Print two integers on two different lines:
+
+The first integer: the number of apples that fall on Sam's house.
+The second integer: the number of oranges that fall on Sam's house.
+
+
+*/
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'countApplesAndOranges' function below.
+     *
+     * The function accepts following parameters:
+     *  1. INTEGER s
+     *  2. INTEGER t
+     *  3. INTEGER a
+     *  4. INTEGER b
+     *  5. INTEGER_ARRAY apples
+     *  6. INTEGER_ARRAY oranges
+     */
+
+    public static void countApplesAndOranges(int s, int t, int a, int b, List<Integer> apples, List<Integer> oranges) {
+    // Write your code here
+    
+    ArrayList<Integer> app = new ArrayList<>();
+    ArrayList<Integer> ora = new ArrayList<>();
+    
+    int tot_app =0;
+    int tot_oran=0;
+    
+    for(int i=0;i<apples.size();i++){
+        int dis_a = apples.get(i)+a;
+        app.add(dis_a);
+        if(app.get(i)>=s && app.get(i)<=t){
+            tot_app++;
+        }
+    }
+    
+    for(int j=0;j<oranges.size();j++){
+        int dis_o = oranges.get(j)+b;
+        ora.add(dis_o);
+        if(ora.get(j)>=s && ora.get(j)<=t){
+            tot_oran++;
+        }
+    }
+    System.out.println(tot_app);
+    System.out.println(tot_oran);
+    }
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int s = Integer.parseInt(firstMultipleInput[0]);
+
+        int t = Integer.parseInt(firstMultipleInput[1]);
+
+        String[] secondMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int a = Integer.parseInt(secondMultipleInput[0]);
+
+        int b = Integer.parseInt(secondMultipleInput[1]);
+
+        String[] thirdMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int m = Integer.parseInt(thirdMultipleInput[0]);
+
+        int n = Integer.parseInt(thirdMultipleInput[1]);
+
+        List<Integer> apples = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> oranges = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        Result.countApplesAndOranges(s, t, a, b, apples, oranges);
+
+        bufferedReader.close();
+    }
+}
