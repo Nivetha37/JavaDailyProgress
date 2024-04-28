c517bb6801e19985b95118435064dd7f990dda06
Nivetha37 <124027942+Nivetha37@users.noreply.github.com>
2024-04-28 20:30:25 +0530

    Create 0.7 Basic_Math.java

---
+++ 0.7 Basic_Math.java
@@ -0,0 +1,25 @@
+/*
+In the new CodeChef Learn module, under the "Learn Problem Solving" section, there are two courses for each language.
+
+For eg. "Python Beginner - Part 1" and "Python Beginner - Part 2". These courses help you get started with CodeChef contests.
+
+Currently there are courses for 4 languages, and hence there are 8 courses in this section.
+
+But suppose there are courses for N languages, what will be the total number of courses in this section?
+  */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner m = new Scanner(System.in);
+		int n = m.nextInt();
+		System.out.println(n*2);
+
+	}
+}
