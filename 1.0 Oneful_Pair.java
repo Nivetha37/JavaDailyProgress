8b770a381889b99dd5b99a96457bab5637d7fef9
Nivetha37 <124027942+Nivetha37@users.noreply.github.com>
2024-05-01 22:37:48 +0530

    Create 1.0 Oneful_Pair.java

---
+++ 1.0 Oneful_Pair.java
@@ -0,0 +1,27 @@
+/* 
+Chef defines a pair of positive integers (a,b) to be a Oneful Pair, if (a+b+(a.b)) . 
+Given two positive integers a and b, output Yes if they are a Oneful Pair. And No otherwise.
+*/
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		if(a+b+(a*b) == 111){
+		    System.out.println("Yes");
+		}
+		else{
+		    System.out.println("No");
+		}
+		
+	}
+}
