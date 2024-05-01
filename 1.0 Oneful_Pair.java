4348325af7e6474ee4d75b8bb46eef9211f8e811
Nivetha37 <124027942+Nivetha37@users.noreply.github.com>
2024-05-01 22:38:55 +0530

    Update 1.0 Oneful_Pair.java

--- 1.0 Oneful_Pair.java
+++ 1.0 Oneful_Pair.java
@@ -1,5 +1,5 @@
 /* 
-Chef defines a pair of positive integers (a,b) to be a Oneful Pair, if (a+b+(a.b)) . 
+Chef defines a pair of positive integers (a,b) to be a Oneful Pair, if (a+b+(a.b)) == 111. 
 Given two positive integers a and b, output Yes if they are a Oneful Pair. And No otherwise.
 */
 
