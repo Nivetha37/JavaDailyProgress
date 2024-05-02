abba0a624f72fa264406e5a3516901c91b8a89e3
Nivetha37 <124027942+Nivetha37@users.noreply.github.com>
2024-05-02 15:08:56 +0530

    Create 1.1 Add_Two_Numbers.java

---
+++ 1.1 Add_Two_Numbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class CodeChef{
+    public static void main(String[] args){
+        Scanner m = new Scanner(System.in);
+        int t = m.nextInt();
+        while(t-- > 0){
+            int a= m.nextInt();
+            int b = m.nextInt();
+            System.out.println(a+b);
+        }
+    }
+}
