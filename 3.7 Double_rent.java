bcf0b04db586019d535c3932883c3c611fe9da41
Nivetha37 <124027942+Nivetha37@users.noreply.github.com>
2024-05-26 18:50:04 +0530

    Create 3.7 Double_rent.java

---
+++ 3.7 Double_rent.java
@@ -0,0 +1,20 @@
+/*
+Chefina decided to move into Chef's apartment.
+Chef was initially paying a rent of X rupees. 
+Since Chefina is moving in, the owner decided to double the rent.
+Find the final rent Chef needs to pay.
+*/
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner m = new Scanner(System.in);
+		int x = m.nextInt();
+		System.out.println(2*x);
+	}
+}
