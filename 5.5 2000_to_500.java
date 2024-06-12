5f44f24a88e059688413aab963fe1e379486820d
Nivetha37 <124027942+Nivetha37@users.noreply.github.com>
2024-06-12 21:02:07 +0530

    Create 5.5 2000_to_500.java

---
+++ 5.5 2000_to_500.java
@@ -0,0 +1,18 @@
+/*Chef had collected N notes of Rs.2000 to pay his total college fees.
+ However, the government banned Rs.2000 notes.
+ Chef wants to pay the same amount using Rs.500 notes only. 
+ Find the number of notes Chef needs.*/
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner m = new Scanner(System.in);
+		int n = m.nextInt();
+		System.out.println(4*n);
+	}
+}
