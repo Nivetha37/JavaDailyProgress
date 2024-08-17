00c520782f6591ee8043d8c988f7384e1467a931
Nivetha37 <124027942+Nivetha37@users.noreply.github.com>
2024-08-17 19:26:12 +0530

    Create 12.5 Discount.java

---
+++ 12.5 Discount.java
@@ -0,0 +1,21 @@
+/*Alice buys a toy with a selling price of 100 rupees.
+There is a discount of x percent on the toy. 
+Find the amount Alice needs to pay for it.*/
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner m = new Scanner(System.in);
+		int t = m.nextInt();
+		while(t-->0){
+		    int x = m.nextInt();
+		    System.out.println(100-x);
+		}
+
+	}
+}
