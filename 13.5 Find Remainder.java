1737a39bbc7bd608566c74e65e0b351fe4d2958c
Nivetha37 <124027942+Nivetha37@users.noreply.github.com>
2024-08-26 18:54:28 +0530

    Create 13.5 Find Remainder.java

---
+++ 13.5 Find Remainder.java
@@ -0,0 +1,20 @@
+/*Write a program to find the remainder when an integer A is divided by an integer B.*/
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner m = new Scanner(System.in);
+		int t = m.nextInt();
+		while(t-->0){
+		    int a = m.nextInt();
+		    int b = m.nextInt();
+		    System.out.println(a%b);
+		}
+
+	}
+}
