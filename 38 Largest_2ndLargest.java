77eac78d021b2c6e2f81ecb714780ec42caef1fa
Nivetha37 <124027942+Nivetha37@users.noreply.github.com>
2024-09-08 11:55:08 +0530

    Update 38 Largest_2ndLargest.java

--- 38 Largest_2ndLargest.java
+++ 38 Largest_2ndLargest.java
@@ -35,3 +35,31 @@ public class Main {
         }
     }
 }
+
+
+
+
+
+/*
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+    public static void main(String[] args) throws java.lang.Exception
+    {
+        // your code goes here
+        Scanner m = new Scanner(System.in);
+        int n = m.nextInt();
+        while (n-- > 0) {
+            int a = m.nextInt();
+            int b = m.nextInt();
+            int c = m.nextInt();
+
+            int[] arr = {a,b,c};
+            Arrays.sort(arr);
+            System.out.println(arr[0] + arr[1]);
+        }
+    }
+}*/
