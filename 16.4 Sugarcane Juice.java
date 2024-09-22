e265acc2775b5b2f220f0c1a2ad0e116bef06a06
Nivetha37 <124027942+Nivetha37@users.noreply.github.com>
2024-09-22 10:26:34 +0530

    Create 16.4 Sugarcane Juice.java

---
+++ 16.4 Sugarcane Juice.java
@@ -0,0 +1,26 @@
+/* While Alice was drinking sugarcane juice, she started wondering about the following facts:
+
+The juicer sells each glass of sugarcane juice for 50 coins.
+He spends 20% of his total income on buying sugarcane.
+He spends 20% of his total income on buying salt and mint leaves.
+He spends 30% of his total income on shop rent.
+Alice wonders, what is the juicer's profit (in coins) when he sells N glasses of sugarcane juice? */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner m = new Scanner(System.in);
+		int t = m.nextInt();
+		while(t-->0){
+		    int n = m.nextInt();
+		    
+		    System.out.println(n*15);
+		}
+	}
+}
