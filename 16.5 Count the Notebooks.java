16f577f0fa825d9fec056413f314461334e2118a
Nivetha37 <124027942+Nivetha37@users.noreply.github.com>
2024-09-23 20:37:09 +0530

    Create 16.5 Count the Notebooks.java

---
+++ 16.5 Count the Notebooks.java
@@ -0,0 +1,22 @@
+/*You know that 1 kg of pulp can be used to make 1000 pages and 1 notebook consists of 100 pages.
+
+Suppose a notebook factory receives N kg of pulp, how many notebooks can be made from that?*/
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner m = new Scanner(System.in);
+		
+		int t = m.nextInt();
+		while(t-->0){
+		    int n = m.nextInt();
+		    System.out.println(n*10);
+		}
+	}
+}
