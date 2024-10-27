53efaca7332efa866b997e2cc06f76f696b1e58b
Nivetha37 <124027942+Nivetha37@users.noreply.github.com>
2024-10-27 21:10:56 +0530

    Create 19.9 Finding Square Roots.java

---
+++ 19.9 Finding Square Roots.java
@@ -0,0 +1,21 @@
+/*In olden days finding square roots seemed to be difficult but nowadays it can be easily done using in-built functions available across many languages .
+
+Assume that you happen to hear the above words and you want to give a try in finding the square root of any given integer using in-built functions. So here's your chance.*/
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner m = new Scanner(System.in);
+		int t = m.nextInt();
+		while(t-->0){
+		    int n = m.nextInt();
+		    System.out.println((int)Math.sqrt(n));
+		}
+
+	}
+}
