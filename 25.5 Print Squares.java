5e0fb3c4a9ec2bd74946fd8bce95288208148074
Nivetha37 <124027942+Nivetha37@users.noreply.github.com>
2024-12-16 19:29:58 +0530

    Create 25.5 Print Squares.java

---
+++ 25.5 Print Squares.java
@@ -0,0 +1,14 @@
+/*Write a program that utilizes a while loop to print the squares of numbers from 1 to 5.*/
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		// your code goes here
+		int i=1;
+		while(i<6){
+		    System.out.print(i*i+" ");
+		    i++;
+		}
+
+	}
+}
