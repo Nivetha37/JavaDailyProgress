74c48695cec3e1a31c2da976b4714539962deb3b
Nivetha37 <124027942+Nivetha37@users.noreply.github.com>
2024-12-18 20:48:18 +0530

    Create 25.7 Convert Temperature.java

---
+++ 25.7 Convert Temperature.java
@@ -0,0 +1,10 @@
+/*Declare a variable "temperature" and initialise it with a value of 25.5 (in Celsius) and Print it in Celsius and Kelvin(add 273 to temperature in Celsius).
+Print the output in the same format as given below.(there is a single space around - (hyphen))*/
+public class Main {
+    public static void main(String[] args) {
+        // Your code goes here
+        double temperature = 25.5;
+        System.out.println("Celsius - "+temperature);
+        System.out.println("Kelvin - "+(temperature+273));
+    }
+}
