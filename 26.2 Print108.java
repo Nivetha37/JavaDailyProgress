e783165313822924b895aa9ce395e9aafa3a61e9
Nivetha37 <124027942+Nivetha37@users.noreply.github.com>
2024-12-22 19:03:55 +0530

    Create 26.2 Print108.java

---
+++ 26.2 Print108.java
@@ -0,0 +1,7 @@
+/*Print the number 108 using arithmetic operations on the numbers 9 and 12.*/
+public class Main {
+    public static void main(String[] args) {
+        // Write your code below
+        System.out.println(9*12);
+    }
+}
