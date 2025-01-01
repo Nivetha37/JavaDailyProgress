16b085de836ccb305c1c1eaf605b94e3382f806b
Nivetha37 <124027942+Nivetha37@users.noreply.github.com>
2025-01-01 23:39:54 +0530

    Create 26.6 Print numbers in reverse.java

---
+++ 26.6 Print numbers in reverse.java
@@ -0,0 +1,15 @@
+/*Printing numbers 10 - 0*/
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+	   // Update the blank given below
+		int i =10;
+        while(i>=0){
+            System.out.println(i);
+            i--;
+        }
+    }
+}
