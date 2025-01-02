64f19ecf2fad33625cebedae08fcf440e921c094
Nivetha37 <124027942+Nivetha37@users.noreply.github.com>
2025-01-02 20:29:25 +0530

    Create 26.7 Print Squares.java

---
+++ 26.7 Print Squares.java
@@ -0,0 +1,12 @@
+/*Write a program to output the squares (using multiplication) of numbers from 1 to 5 on separate lines.*/
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		
+		for(int i=1;i<6;i++){
+		    System.out.println(i+" - "+(i*i));
+		}
+
+	}
+}
