9429f1c5ce5761b2c6b843f3f5fb8cf9904c527f
Nivetha37 <124027942+Nivetha37@users.noreply.github.com>
2025-02-22 21:49:30 +0530

    Create 108 Palindrome Number.java

---
+++ 108 Palindrome Number.java
@@ -0,0 +1,15 @@
+/*Given an integer x, return true if x is a palindrome, and false otherwise.*/
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        
+        if(x<0 ||(x%10==0 && x!=0)) return false;
+        int rev =0;
+        while(x>rev){
+            rev=rev*10+x%10;
+            x/=10;
+        }
+
+        return x==rev || x == rev/10;
+    }
+}
