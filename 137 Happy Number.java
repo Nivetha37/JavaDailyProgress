424284e06c113e075376ea8a7af1c2ace075c511
Nivetha37 <124027942+Nivetha37@users.noreply.github.com>
2025-03-24 21:10:37 +0530

    Create 137 Happy Number.java

---
+++ 137 Happy Number.java
@@ -0,0 +1,60 @@
+/*Write an algorithm to determine if a number n is happy.
+
+A happy number is a number defined by the following process:
+
+Starting with any positive integer, replace the number by the sum of the squares of its digits.
+Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+Those numbers for which this process ends in 1 are happy.
+Return true if n is a happy number, and false if not.
+
+class Solution {
+    static int Squaresum(int n){
+        int sum =0;
+
+        while(n>0){
+            int dig = n%10;
+            sum+=(dig*dig);
+            n/=10;
+        }
+        return sum;
+    }
+    public boolean isHappy(int n) {
+        
+        Set<Integer> s = new HashSet<>();
+        while(n!=1){
+            n = Squaresum(n);
+            if(s.contains(n)) return false;
+            s.add(n);
+        }
+
+        return true;
+          
+
+    }
+}
+
+ */
+class Solution {
+    static int Squaresum(int n){
+        int sum =0;
+
+        while(n>0){
+            int dig = n%10;
+            sum+=(dig*dig);
+            n/=10;
+        }
+        return sum;
+    }
+    public boolean isHappy(int n) {
+        
+        int slow = n,fast=n;
+        do{
+            slow = Squaresum(slow);
+            fast = Squaresum(Squaresum(fast));
+        }while(slow!=fast);
+
+        return slow==1;
+          
+
+    }
+}
