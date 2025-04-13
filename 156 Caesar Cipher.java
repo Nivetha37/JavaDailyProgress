9fc4802b1f71d6dfc48e0d9de96ce64d50d92872
Nivetha37 <124027942+Nivetha37@users.noreply.github.com>
2025-04-13 21:51:23 +0530

    Create 156 Caesar Cipher.java

---
+++ 156 Caesar Cipher.java
@@ -0,0 +1,62 @@
+/*Julius Caesar protected his confidential information by encrypting it using a cipher. Caesar's cipher shifts each letter by a number of letters. If the shift takes you past the end of the alphabet, just rotate back to the front of the alphabet. In the case of a rotation by 3, w, x, y and z would map to z, a, b and c.
+
+Original alphabet:      abcdefghijklmnopqrstuvwxyz
+Alphabet rotated +3:    defghijklmnopqrstuvwxyzabc
+Example
+
+
+The alphabet is rotated by , matching the mapping above. The encrypted string is .
+
+Note: The cipher only encrypts letters; symbols, such as -, remain unencrypted.*/
+
+import java.io.*;
+
+class Result {
+
+    /*
+     * Complete the 'caesarCipher' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. STRING s
+     *  2. INTEGER k
+     */
+
+    public static String caesarCipher(String s, int k) {
+    // Write your code here
+    k = k%26;
+    String res="" ;
+    
+    for(char c : s.toCharArray()){
+        if(Character.isLetter(c)){
+            char base = Character.isUpperCase(c)?'A':'a';
+            res+=((char)((c-base+k)%26+base));
+        }
+        else res+=c;
+    }
+    
+    return res;
+    }   
+
+}
+            
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String s = bufferedReader.readLine();
+
+        int k = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String result = Result.caesarCipher(s, k);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
